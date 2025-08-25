a6c662ee06ccd212d1ef69e4a225138222bc0844
Lohitaksha <patarylohitaksha06@gmail.com>
2025-08-25 16:41:01 +0530

    rough strucutre

---
+++ backend/src/main/java/com/example/digitalporch/DigitalPorchApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitalporch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalPorchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalPorchApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Digital Porch</h1>
+    </>
+  )
+}
+
+export default App
